58f5a1eabb5566eaa54c3252e9de051861941d72
ngocbau12324 <ngocbau2015tqk@gmail.com>
2022-05-01 11:25:58 +0700

    lan1

---
+++ cart.php
@@ -0,0 +1,113 @@
+<div class="cart">
+    <div class="cart-sidebar">
+        <div class="page-title-name">
+            Giỏ hàng
+        </div>
+        <div class="breadcumb">Cửa hàng / Giỏ hàng</div>
+        <div class="cart-products">
+            <div class="cart-products-inner">
+                <div class="cart-item">
+                    <table>
+                        <tr>
+                            <td rowspan="3" style="width:15%;">
+                                <div class="item-image">
+                                    <img src="https://d2j6dbq0eux0bg.cloudfront.net/images/73605784/2991317837.jpg"
+                                        alt="Item image">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="item-title">
+                                    TS Gucci
+                                </div>
+                            </td>
+                            <td>
+                                <div class="item-control-delete ec-link-hover" style="">X</div>
+                            </td>
+                        </tr>
+                        <tr>
+
+                            <td style="color:#757575">
+                                <div class="item-option">
+                                    <div class="item-option-size">Product Size:S</div>&nbsp;
+                                    <div class="item-option-color">Color:Black</div>
+                                </div>
+
+                            </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td style="color:#1a7ac4">
+                                <div class="item-qty ec-link-hover">Số lượng: 1</div>
+                            </td>
+                            <td style="text-align:right;">
+                                <div class="item-price">280&nbsp;200đ</div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+
+            </div>
+            <hr>
+        </div>
+        <div class="cart-summary">
+            <table class="cart-summary-body">
+                <tbody>
+                    <tr>
+                        <td class="cart-summary-title">Tổng phụ</td>
+                        <td class="cart-summary-price">220&nbsp;200đ</td>
+                    </tr>
+                    <tr>
+                        <td class="cart-summary-title">Giao hàng</td>
+                        <td class="cart-summary-price">Miễn phí</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr class="cart-summary-row-total">
+                        <td class="cart-summary-title">
+                            TỔNG CỘNG
+                        </td>
+                        <td class="cart-summary-price cart-summary-price-total">
+                            220&nbsp;200đ
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="cart-shopping">
+            Tìm kiếm thêm?
+            <a href="" class="ec-link-hover"> Tiếp tục mua hàng</a>
+
+        </div>
+    </div>
+    <div class="cart-body">
+        <div class="page-title-name">
+            Thanh toán
+        </div>
+        <div class="cart-email">
+            <div class="cart-email_text">
+                Nhập địa chỉ email của bạn. Địa chỉ này sẽ được sử dụng để gửi cho bạn thông tin cập nhật về đơn hàng
+                của bạn.
+            </div>
+            <form action="" class="cart-email_input">
+                <div>
+                    <input type="email" name="email" id="cart-email-input" placeholder="Địa chỉ email của bạn">
+                </div>
+                <div>
+
+                </div>
+            </form>
+        </div>
+        <div class="cart_checkout">
+            <div class="cart_agreement">
+                <div class="form-checkbox">
+                    <input type="checkbox" name="" id="form-control_checkbox-accept-marketing">
+                </div>
+                <div class="form-checkbox-lable">
+                    <label for="form-control_checkbox-accept-marketing">Luôn cập nhật tin tức và các ưu đãi độc quyền
+                        cho tôi</label>
+                </div>
+            </div>
+        </div>
+        <div class="cart_next"></div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css" type="text/css">
+    <!-- Proxima Nova reference. -->
+    <link href="https://use.typekit.net/emv3zbo.css" rel="stylesheet" crossorigin="anonymous">
+    <!-- Arizona Bootstrap reference. -->
+    <link rel="stylesheet"
+        href="https://cdn.digital.arizona.edu/lib/arizona-bootstrap/2.0.13/css/arizona-bootstrap.min.css"
+        crossorigin="anonymous">
+    
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="header">
+
+        </div>
+        <div class="content">
+            <?php
+            include('./cart.php')
+            ?>
+        </div>
+        <div class="footer">
+
+        </div>
+    </div>
+</body>
+
+</html>
